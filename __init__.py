e9fe34058045019ee04298cb7ab65fe3577d25be
mrillies <jacob.gillies@gmail.com>
2018-08-26 18:59:23 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,35 +5,40 @@ import time
 from modules.core.props import Property, StepProperty
 from modules.core.step import StepBase
 from modules import cbpi
+import traceback
 
 @cbpi.step
 class AutoSwitch(StepBase):
 
     # Properties
-    kettle = StepProperty.Kettle("Kettle")
+    id = StepProperty.Kettle("Kettle")
     auto_type = Property.Select("Auto Setting", options=["On", "Off"])
 
     def init(self):
-        kettle = cbpi.cache.get("kettle")[id]
-        if kettle.state is False:
-            # Start controller
-            if kettle.logic is not None:
-                cfg = kettle.config.copy()
-                cfg.update(dict(api=cbpi, kettle_id=kettle.id, heater=kettle.heater, sensor=kettle.sensor))
-                instance = cbpi.get_controller(kettle.logic).get("class")(**cfg)
-                instance.init()
-                kettle.instance = instance
-                def run(instance):
-                    instance.run()
-                t = self.api.socketio.start_background_task(target=run, instance=instance)
-            kettle.state = not kettle.state
-            cbpi.emit("UPDATE_KETTLE", cbpi.cache.get("kettle").get(id))
-        else:
-            # Stop controller
-            kettle.instance.stop()
-            kettle.state = not kettle.state
-            cbpi.emit("UPDATE_KETTLE", cbpi.cache.get("kettle").get(id))
-
+        try:
+            kettle = cbpi.cache.get("kettle")[self.id]
+            if (kettle.state is False) and (self.auto_type = "On"):
+                # Start controller
+                if kettle.logic is not None:
+                    cfg = kettle.config.copy()
+                    cfg.update(dict(api=cbpi, kettle_id=kettle.id, heater=kettle.heater, sensor=kettle.sensor))
+                    instance = cbpi.get_controller(kettle.logic).get("class")(**cfg)
+                    instance.init()
+                    kettle.instance = instance
+                    def run(instance):
+                        instance.run()
+                    t = self.api.socketio.start_background_task(target=run, instance=instance)
+                kettle.state = not kettle.state
+                cbpi.emit("UPDATE_KETTLE", cbpi.cache.get("kettle").get(id))
+            else (kettle.state is True) and (self.auto_type = "Off"):
+                # Stop controller
+                kettle.instance.stop()
+                kettle.state = not kettle.state
+                cbpi.emit("UPDATE_KETTLE", cbpi.cache.get("kettle").get(id))
+        catch Exception as e:
+            cbpi.notify("Auto Error", "Failed to switch auto", type="danger", timeout=0)
+            traceback.print_exc()
+            
 	def finish(self):
 		pass
 
